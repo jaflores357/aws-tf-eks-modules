7971691b924e86d69e00314fac7a27af7e4737f6
Jorge Flores <jorge.flores@syntonic.com>
2020-08-31 11:56:44 -0300

    Fix redis script

--- redis.tf
+++ redis.tf
@@ -12,26 +12,5 @@ resource "aws_elasticache_cluster" "redis" {
   engine_version       = "3.2.10"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis.name
-  security_group_names = [aws_security_group.redis.name]
 }
 
-resource "aws_security_group" "redis" {
-  name        =  "${local.environment}-redis-sg"
-  description = "Allow "
-
-  ingress {
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = local.private_cidr_blocks
-    description = "Redis access"
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-}
